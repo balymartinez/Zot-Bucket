cd4f06d9baa93b086cfcff3b4cf41f193f482a6a
MiaNguyen912 <thuydn7@uci.edu>
2023-11-04 13:46:40 -0700

    add functionality

--- uci-map.js
+++ uci-map.js
@@ -6,3 +6,54 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//---------------------------------
+
+const restaurants = [
+    { name: 'Restaurant 1', location: "", url: 'https://example.com/restaurant1' },
+    { name: 'Restaurant 2', location: "", url: 'https://example.com/restaurant2' },
+    { name: 'Restaurant 3', location: "", url: 'https://example.com/restaurant3' }
+];
+
+const studySpaces = [
+    { name: 'Langson Library', location: "", url: 'https://example.com/restaurant1' },
+    { name: 'Law Library', location: "", url: 'https://example.com/restaurant2' },
+    { name: 'Science Library ', location: "", url: 'https://example.com/restaurant3' }
+];
+
+
+
+//---------------------------------
+
+
+function showItem(item) {
+    let list = document.querySelector('ul');
+    let list_item = document.createElement("li");
+    const button = document.createElement('button');
+    button.textContent = item.name;
+    list_item.appendChild(button);
+    list.appendChild(list_item);
+}
+function clearItems(item){
+    let list = document.querySelector('#items');
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+function showRestaurants(){
+    clearItems();
+    restaurants.forEach(showItem); // Use the forEach() method to call the function on each item
+
+}
+
+function showStudySpaces(){
+    clearItems();
+    studySpaces.forEach(showItem); // Use the forEach() method to call the function on each item
+
+}
+
+function showClubsAnsEvents(){
+    clearItems();
+    // restaurants.forEach(showItem); // Use the forEach() method to call the function on each item
+
+}
