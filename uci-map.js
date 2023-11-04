ed6ff30274ef1fb7fead28146add975466baecd6
Baly Martinez <balym@uci.edu>
2023-11-04 15:20:46 -0700

    added url for restraunts

--- uci-map.js
+++ uci-map.js
@@ -9,22 +9,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //---------------------------------
 
 const restaurants = [
-    { name: 'Brandywine', location: [33.64540329022346, -117.83928262177889]},
-    { name: 'Java City Kiosk', location: [33.64342086082128, -117.84115637545828]},
-    { name: 'Med Ed Cafe', location: [33.64452215740989, -117.85189061002549]},
-    { name: 'Phoenix Food Court', location: [33.645599107261454, -117.8407076396062]},
-    { name: 'Starbucks @ Biological Sciences', location: [33.64498862746285, -117.84564024802606]},
-    { name: 'Starbucks @ Paul Merage School of Business', location: [33.64663727961053, -117.83803714082919]},
-    { name: 'Anthill Pub', location: [33.64893997674284, -117.8422533219668]},
-    { name: 'Bento', location: [33.648830986166416, -117.84210095478684]},
-    { name: 'Jamba', location: [33.648830986166416, -117.84210095478684]},
-    { name: 'Panda Express', location: [33.648899339073196, -117.84208400857327]},
-    { name: 'Planteatery', location: [33.648899339073196, -117.84208400857327]},
-    { name: 'Subway', location: [33.64925284884054, -117.84247968566768]},
-    { name: 'ZotnGo Market', location: [33.64872449938839, -117.84276830100971]},
-    { name: 'The Anteatery', location: [33.65119240860278, -117.84499118571881]},
-    { name: 'The Green Room', location: [33.64980183961821, -117.84472431876374]},
-    { name: 'University Club (UN CLB)', location: [33.643775880392305, -117.8420042322584]},
+    { name: 'Brandywine', location: [33.64540329022346, -117.83928262177889], url: "https://uci.campusdish.com/en/LocationsAndMenus/Brandywine"},
+    { name: 'Java City Kiosk', location: [33.64342086082128, -117.84115637545828], url: "https://uci.campusdish.com/LocationsAndMenus/JavaCityKiosk"},
+    { name: 'Med Ed Cafe', location: [33.64452215740989, -117.85189061002549], url: "https://uci.campusdish.com/LocationsAndMenus/MedEdCafe" },
+    { name: 'Phoenix Food Court', location: [33.645599107261454, -117.8407076396062], url: "https://uci.campusdish.com/LocationsAndMenus/PhoenixFoodCourt" },
+    { name: 'Starbucks @ Biological Sciences', location: [33.64498862746285, -117.84564024802606] , url: "https://uci.campusdish.com/LocationsAndMenus/StarbucksBiologicalSciences" },
+    { name: 'Starbucks @ Paul Merage School of Business', location: [33.64663727961053, -117.83803714082919], url: "https://uci.campusdish.com/LocationsAndMenus/StarbucksPaulMerageSchoolofBusiness" },
+    { name: 'Anthill Pub', location: [33.64893997674284, -117.8422533219668], url: "https://uci.campusdish.com/LocationsAndMenus/StudentCenter/AnthillPubGrille" },
+    { name: 'Bento', location: [33.648830986166416, -117.84210095478684], url: "https://uci.campusdish.com/LocationsAndMenus/StudentCenter/BentoSushi" },
+    { name: 'Jamba', location: [33.648830986166416, -117.84210095478684], url: "https://uci.campusdish.com/LocationsAndMenus/StudentCenter/JambaJuice" },
+    { name: 'Panda Express', location: [33.648899339073196, -117.84208400857327], url: "https://uci.campusdish.com/LocationsAndMenus/StudentCenter/PandaExpress" },
+    { name: 'Planteatery', location: [33.648899339073196, -117.84208400857327], url: "https://uci.campusdish.com/LocationsAndMenus/StudentCenter/Planteatery" },
+    { name: 'Subway', location: [33.64925284884054, -117.84247968566768], url: "https://uci.campusdish.com/LocationsAndMenus/StudentCenter/Subway" },
+    { name: 'ZotnGo Market', location: [33.64872449938839, -117.84276830100971], url: "https://uci.campusdish.com/LocationsAndMenus/ZotNGoMarket" },
+    { name: 'The Anteatery', location: [33.65119240860278, -117.84499118571881], url: "https://uci.campusdish.com/LocationsAndMenus/TheAnteatery" },
+    { name: 'The Green Room', location: [33.64980183961821, -117.84472431876374], url: "https://uci.campusdish.com/LocationsAndMenus/TheGreenRoom" },
+    { name: 'University Club (UN CLB)', location: [33.643775880392305, -117.8420042322584], url: "https://www.uclub.uci.edu/" },
 ];
 
 
