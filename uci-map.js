50cdee3a2957932931806e60b838d3166143bce5
MiaNguyen912 <thuydn7@uci.edu>
2023-11-04 14:08:44 -0700

    add function to show name of each item when clicked

--- uci-map.js
+++ uci-map.js
@@ -9,10 +9,45 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //---------------------------------
 
 const restaurants = [
-    { name: 'Brandywine', location: "", url: 'https://example.com/restaurant1' },
-    { name: 'Restaurant 2', location: "", url: 'https://example.com/restaurant2' },
-    { name: 'Restaurant 3', location: "", url: 'https://example.com/restaurant3' }
+    { name: 'Brandywine', location: "33.64540329022346, -117.83928262177889"},
+    { name: 'Java City Kiosk', location: "33.64342086082128, -117.84115637545828"},
+    { name: 'Med Ed Cafe', location: " 33.64452215740989, -117.85189061002549"},
+    { name: 'Phoenix Food Court', location: " 33.645599107261454, -117.8407076396062"},
+    { name: 'Starbucks @ Biological Sciences', location: "33.64498862746285, -117.84564024802606"},
+    { name: 'Starbucks @ Paul Merage School of Business', location: "33.64663727961053, -117.83803714082919"},
+    { name: 'Anthill Pub', location: "33.64893997674284, -117.8422533219668"},
+    { name: 'Bento', location: "33.648830986166416, -117.84210095478684"},
+    { name: 'Jamba', location: "33.648830986166416, -117.84210095478684"},
+    { name: 'Panda Express', location: "33.648899339073196, -117.84208400857327"},
+    { name: '', location: ""},
+
+
+
 ];
+
+
+ 
+
+
+
+ 
+
+ 
+
+// Planteatery
+// Starbucks
+// Subway
+// 33.64925284884054, -117.84247968566768
+// ZotnGo Market
+// 33.64872449938839, -117.84276830100971
+// Taza
+// The Anteatery
+// 33.65119240860278, -117.84499118571881
+// The Green Room
+// 33.64980183961821, -117.84472431876374
+// University Club (UN CLB)
+// 33.643775880392305, -117.8420042322584
+
 
 const studySpaces = [
     { name: 'Langson Library', location: "", url: 'https://example.com/restaurant1' },
@@ -21,19 +56,19 @@ const studySpaces = [
 ];
 
 
-
 //---------------------------------
-
-
 function showItem(item) {
     let list = document.querySelector('ul');
     let list_item = document.createElement("li");
     const button = document.createElement('button');
     button.textContent = item.name;
-    button.addEventListener('click', showInMap);
+    button.addEventListener('click', function(){
+        showInMap(button.textContent)
+    });
     list_item.appendChild(button);
     list.appendChild(list_item);
 }
+
 function clearItems(item){
     let list = document.querySelector('#items');
     while (list.firstChild) {
@@ -60,6 +95,6 @@ function showClubsAnsEvents(){
 }
 
 
-function showInMap(){
-    alert("hello")
+function showInMap(itemName){
+    alert(itemName)
 }
